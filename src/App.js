aebd2859bcbccb0cb4efe0f2c740d26b65bfd3c5
Henrique Zago <lobato.zago@gmail.com>
2020-08-27 15:20:09 +0100

    Updates App component to display blog posts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BlogPost from './components/BlogPost/BlogPost';
+
+import './App.scss';
+
+export const POSTS_ENDPOINT_URL = 'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      blogPosts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(POSTS_ENDPOINT_URL)
+        .then(response => response.json())
+        .then(blogPosts => this.setState(
+            {blogPosts: this.parsePosts(blogPosts)}));
+  }
+
+  parsePosts(blogPosts) {
+    let index = 0;
+    const result = [];
+
+    while (index < blogPosts.length) {
+      if (index % 3 === 0) result.push([]);
+      result[result.length - 1].push(blogPosts[index++]);
+    }
+
+    return result;
+  }
+
+  renderPosts() {
+    return this.state.blogPosts.map((blogPosts, index) => (
+        <div className='row' key={index}>
+          {
+            blogPosts
+                .map(blogPost => (
+                    <BlogPost key={blogPost.id} blogPost={blogPost}/>))
+          }
+        </div>
+    ));
+  }
+
+  render() {
+    return (
+        <div className="posts-container">
+          {this.renderPosts()}
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import {render} from '@testing-library/react';
+import {waitForDomChange} from '@testing-library/dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App, {POSTS_ENDPOINT_URL} from './App';
+import BlogPost from './components/BlogPost/BlogPost';
+import sampleResponse from './sample-response.json';
+
+describe('App', () => {
+  beforeEach(() => {
+    jest.spyOn(window, 'fetch');
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => sampleResponse
+    });
+  });
+
+  test('renders blog posts', async () => {
+    const result = render(<App/>, {hydrate: false});
+    const {container} = result;
+
+    expect(window.fetch).toHaveBeenCalledWith(POSTS_ENDPOINT_URL);
+
+    await waitForDomChange(() => {
+      const rows = container.getElementsByClassName('row');
+      const blogPosts = container.getElementsByTagName(BlogPost);
+      expect(rows).toHaveLength(1);
+      expect(blogPosts).toHaveLength(3);
+    });
+  });
 });
